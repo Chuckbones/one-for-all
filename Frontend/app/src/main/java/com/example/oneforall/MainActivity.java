087f404a702ca74b7c1e6b17bd16500b9d44216b
Rudransh <jauharirudransh@gmail.com>
2022-03-13 02:27:41 +0530

    Making Different activities

--- Frontend/app/src/main/java/com/example/oneforall/MainActivity.java
+++ Frontend/app/src/main/java/com/example/oneforall/MainActivity.java
@@ -1,226 +1,58 @@
 package com.example.oneforall;
-
-import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import java.io.*;
-import android.os.Environment;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private static final String READ_EXTERNAL_STORAGE = "1";
-    private static final String WRITE_EXTERNAL_STORAGE = "1";
     String path;
-
-    String[] permission= {READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE};
     private final String url="https://9a6d-106-212-107-29.ngrok.io";
 
-    ActivityResultLauncher <Intent> activityResultLauncher;
-    ActivityResultLauncher <Intent> activityResult;
-
+    ActivityResultLauncher <String> activityResult = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+        @Override
+        public void onActivityResult(Uri result) {
+            path = result.toString();
+            readActivity();
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ConvertToPdf= findViewById(R.id.button);
 
-        TextView textView = findViewById(R.id.textView);
-        Button Browse = findViewById(R.id.button2);
-        Browse.setOnClickListener(new View.OnClickListener() {
+        ConvertToPdf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Filechooser();
-            }
-        });
-        activityResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
-                if(result.getResultCode()== Activity.RESULT_OK){
-                    path = getIntent().getData().toString();
                 }
-            }
-        });
-
-        String tContents = " ";
-        File file=new File(path);
-        if(file.exists()){
-            tContents = readfile(path);
-            textView.setText(tContents);
-        }
-
-        Button ConvertToPdf= findViewById(R.id.button);
-
-        //Upload file
-        if (file.exists()) {
-            ConvertToPdf.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (checkPermission()) {
-                        OkHttpClient client = new OkHttpClient();
-                        RequestBody formBody = new MultipartBody.Builder().setType(MultipartBody.FORM).addFormDataPart("file", file.getName(), RequestBody.create(file, MediaType.parse("text/txt"))).build();
-                        Request request = new Request.Builder().url(url).post(formBody).build();
-                        client.newCall(request).enqueue(new Callback() {
-                            @Override
-                            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                                Toast.makeText(MainActivity.this, "network not found", Toast.LENGTH_LONG).show();
-                            }
-
-                            @Override
-                            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                                TextView textView = findViewById(R.id.textView);
-
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        try {
-                                            textView.setText(response.body().string());
-                                        } catch (IOException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                });
-
-                               }
-                           });
-                       }
-                    else{
-                        requestPermission();
-                    }
-                    }
-
-                });
-        }
-        else
-        {
-            textView.setText("File not found");
-        }
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-        @Override
-        public void onActivityResult(ActivityResult result) {
-             if(result.getResultCode()== Activity.RESULT_OK){
-                 if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
-                     if(Environment.isExternalStorageManager()){
-                        Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
-                     }
-                     else{
-                         Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
-                     }
-                 }
-             }
-        }
         });
 
     }
-
-    //check if the initial permission is granted
-    boolean checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            return Environment.isExternalStorageManager();
-        } else {
-            int result = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
-            int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-            return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
-        }
-    }
-
-    //Request permission
-    void requestPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            try {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-                intent.addCategory("android.intent.category.DEFAULT");
-                intent.setData(Uri.parse(String.format("package:%s",new Object[]{getApplicationContext().getPackageName()})));
-                activityResultLauncher.launch(intent);
-            } catch (Exception e) {
-                Intent intent = new Intent();
-                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-                activityResultLauncher.launch(intent);
-            }
-        } else {
-            ActivityCompat.requestPermissions(MainActivity.this, permission,0);
-        }
-    }
-
-    //check the final permission
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 0:
-                if (grantResults.length > 0) {
-                    boolean READ_EXTERNAL_STORAGE = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    boolean WRITE_EXTERNAL_STORAGE = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-
-                    if (READ_EXTERNAL_STORAGE && WRITE_EXTERNAL_STORAGE) {
-                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(this, "You Denied the Permission", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-    }
-
-    //choose a file
     public void Filechooser()
     {
-        Intent i = new Intent (Intent.ACTION_GET_CONTENT);
-        i.setType("*/*");
-        i.addCategory(Intent.CATEGORY_OPENABLE);
-
         try{
-            i.setAction("Choose a file");
-            activityResult.launch(i);
+            activityResult.launch("*/*");
         }
         catch(android.content.ActivityNotFoundException ex){
             Toast.makeText(this, "Please install a file manager", Toast.LENGTH_SHORT).show();
         }
     }
+    public void readActivity(){
 
-    //read file
-    public String readfile(String path){
-        String tContents = "";
-        if(checkPermission()) {
-            try {
-                FileInputStream stream = new FileInputStream(path);
-                int size = stream.available();
-                byte[] buffer = new byte[size];
-                stream.read(buffer);
-                stream.close();
-                tContents = new String(buffer);
-            } catch (IOException e) {
-            }
-        }
-        else{
-            requestPermission();
-        }
-        return tContents;
+        Intent a= new Intent(MainActivity.this, MainActivity2.class);
+        a.putExtra("path",path);
+        startActivity(a);
     }
+
+
 
 }
---
+++ Frontend/app/src/main/java/com/example/oneforall/MainActivity2.java
@@ -0,0 +1,119 @@
+package com.example.oneforall;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainActivity2 extends AppCompatActivity {
+    private static final String READ_EXTERNAL_STORAGE = "1";
+    private static final String WRITE_EXTERNAL_STORAGE = "1";
+    String[] permission= {READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE};
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle a = getIntent().getExtras();
+        String path = a.getString("path");
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(path);
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode()== Activity.RESULT_OK){
+                    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.R){
+                        if(Environment.isExternalStorageManager()){
+                            Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+    }
+    public String readfile(String path){
+        String tContents = "";
+        if(checkPermission()) {
+            try {
+                FileInputStream stream = new FileInputStream(path);
+                int size = stream.available();
+                byte[] buffer = new byte[size];
+                stream.read(buffer);
+                stream.close();
+                tContents = new String(buffer);
+            } catch (IOException e) {
+            }
+        }
+        else {
+            requestPermission();
+        }
+        return tContents;
+    }
+    boolean checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return Environment.isExternalStorageManager();
+        } else {
+            int result = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+            int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+            return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+    void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.addCategory("android.intent.category.DEFAULT");
+                intent.setData(Uri.parse(String.format("package:%s",new Object[]{getApplicationContext().getPackageName()})));
+                activityResultLauncher.launch(intent);
+            } catch (Exception e) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+                activityResultLauncher.launch(intent);
+            }
+        } else {
+            ActivityCompat.requestPermissions(MainActivity2.this, permission,0);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 0:
+                if (grantResults.length > 0) {
+                    boolean READ_EXTERNAL_STORAGE = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean WRITE_EXTERNAL_STORAGE = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (READ_EXTERNAL_STORAGE && WRITE_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "You Denied the Permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+}
