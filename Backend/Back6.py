abd970de7fb10737cb8d402773713aa20e3ff5f0
Chuckbones <shivamojha42@gmail.com>
2022-07-19 02:58:41 +0530

    build: remove unwanted files + style page + add download button (not working)

--- Backend/Back6.py
+++ Backend/Back6.py
@@ -1,7 +1,8 @@
+from fileinput import filename
 import os
 import docx2txt
 
-from flask import Flask , request , redirect ,flash , url_for
+from flask import Flask, request, redirect, flash, url_for, render_template, current_app
 from werkzeug.utils import secure_filename
 from pdf2docx import Converter
 from docx2pdf import convert
@@ -11,43 +12,48 @@ from bs4 import BeautifulSoup
 from fpdf import FPDF
 
 
-
-
 def convert_pdf_to_docx(input_name, output_name):
-  cv = Converter(input_name)
-  cv.convert(output_name)
-  cv.close()
+    cv = Converter(input_name)
+    cv.convert(output_name)
+    cv.close()
 
-def convert_docx_to_pdf(input_name,output_name):
+
+def convert_docx_to_pdf(input_name, output_name):
     docx_file = input_name
     convert(docx_file)
 
-def convert_png_to_pdf(input_name,output_name):
+
+def convert_png_to_pdf(input_name, output_name):
     image_1 = Image.open(input_name)
     im_1 = image_1.convert('RGB')
     UPLOAD_FOLDER = "C:/Upload"
     im_1.save(UPLOAD_FOLDER)
 
-def convert_docx_to_txt(input_name,output_name):
- text = docx2txt.process(input_name) 
- with open(output_name, "w") as text_file:
-	 print(text, file=text_file)
 
-def convert_jpg_to_png(input_name,output_name):
-    
- im1 = Image.open(input_name)
- im1.save(input_name)
+def convert_docx_to_txt(input_name, output_name):
+    text = docx2txt.process(input_name)
+    with open(output_name, "w") as text_file:
+        print(text, file=text_file)
 
-def convert_txt_to_pdf(input_name,output_name):
-    pdf = FPDF()   
+
+def convert_jpg_to_png(input_name, output_name):
+
+    im1 = Image.open(input_name)
+    im1.save(input_name)
+
+
+def convert_txt_to_pdf(input_name, output_name):
+    pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size = 15)
+    pdf.set_font("Arial", size=15)
     f = open(input_name, "r")
     for x in f:
-        pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
+        pdf.cell(200, 10, txt=x, ln=1, align='C')
 
-        pdf.output(output_name) 
-def convert_xlsx_to_pdf(input_name,output_name):
+        pdf.output(output_name)
+
+
+def convert_xlsx_to_pdf(input_name, output_name):
     from win32com import client
 
     input_file = input_name
@@ -61,63 +67,76 @@ def convert_xlsx_to_pdf(input_name,output_name):
     try:
         Workbook.ActiveSheet.ExportAsFixedFormat(0, output_file)
     except Exception as e:
-           print("Failed to convert in PDF format.Please confirm environment meets all the requirements  and try again")
-           print(str(e))
+        print("Failed to convert in PDF format.Please confirm environment meets all the requirements  and try again")
+        print(str(e))
     finally:
-           Workbook.Close()
+        Workbook.Close()
     app.Exit()
-  
+
+
 def convert_file(input_name):
-  filename = input_name.split('.')[0]
-  old_extension = input_name.split('.')[-1]
+    filename = input_name.split('.')[0]
+    old_extension = input_name.split('.')[-1]
+   # extension = input ()
 
-  if old_extension == 'pdf':
-    extension = 'docx'
-    convert_pdf_to_docx(
-      os.path.join(app.config['UPLOAD_FOLDER'], input_name), 
-      os.path.join(app.config['UPLOAD_FOLDER'], filename + '-edited.' + extension)
-      )
-  elif old_extension == 'docx':
-       extension = 'pdf'
-       convert_docx_to_pdf(
-        os.path.join(app.config['UPLOAD_FOLDER'], input_name), 
-        os.path.join(app.config['UPLOAD_FOLDER'], filename + '-edited.' + extension)
+    if old_extension == 'pdf':
+        extension = 'docx'
+        convert_pdf_to_docx(
+            os.path.join(app.config['UPLOAD_FOLDER'], input_name),
+            os.path.join(app.config['UPLOAD_FOLDER'],
+                         filename + '-edited.' + extension)
         )
-  elif old_extension == 'png':
-       extension = 'pdf'
-       convert_png_to_pdf(
-        os.path.join(app.config['UPLOAD_FOLDER'], input_name), 
-        os.path.join(app.config['UPLOAD_FOLDER'], filename + '-edited.' + extension)
+    elif old_extension == 'docx':
+        extension = 'pdf'
+        convert_docx_to_pdf(
+            os.path.join(app.config['UPLOAD_FOLDER'], input_name),
+            os.path.join(app.config['UPLOAD_FOLDER'],
+                         filename + '-edited.' + extension)
         )
-  elif old_extension == 'jpg':
-       extension = 'png'
-       convert_jpg_to_png(
-        os.path.join(app.config['UPLOAD_FOLDER'], input_name), 
-        os.path.join(app.config['UPLOAD_FOLDER'], filename + '-edited.' + extension)
+    elif old_extension == 'png':
+        extension = 'pdf'
+        convert_png_to_pdf(
+            os.path.join(app.config['UPLOAD_FOLDER'], input_name),
+            os.path.join(app.config['UPLOAD_FOLDER'],
+                         filename + '-edited.' + extension)
         )
-  elif old_extension == 'txt':
-       extension = 'pdf'
-       convert_txt_to_pdf(
-        os.path.join(app.config['UPLOAD_FOLDER'], input_name), 
-        os.path.join(app.config['UPLOAD_FOLDER'], filename + '-edited.' + extension)
+    elif old_extension == 'jpg':
+        extension = 'png'
+        convert_jpg_to_png(
+            os.path.join(app.config['UPLOAD_FOLDER'], input_name),
+            os.path.join(app.config['UPLOAD_FOLDER'],
+                         filename + '-edited.' + extension)
         )
-  elif old_extension == 'xlsx':
-       extension = 'pdf'
-       convert_xlsx_to_pdf(
-        os.path.join(app.config['UPLOAD_FOLDER'], input_name), 
-        os.path.join(app.config['UPLOAD_FOLDER'], filename + '-edited.' + extension)
+    elif old_extension == 'txt':
+        extension = 'pdf'
+        convert_txt_to_pdf(
+            os.path.join(app.config['UPLOAD_FOLDER'], input_name),
+            os.path.join(app.config['UPLOAD_FOLDER'],
+                         filename + '-edited.' + extension)
         )
-      
-  else:
-    print('Invalid!!')
+    elif old_extension == 'xlsx':
+        extension = 'pdf'
+        convert_xlsx_to_pdf(
+            os.path.join(app.config['UPLOAD_FOLDER'], input_name),
+            os.path.join(app.config['UPLOAD_FOLDER'],
+                         filename + '-edited.' + extension)
+        )
 
-app=Flask(__name__)
+    else:
+        print('Invalid!!')
 
 
-UPLOAD_FOLDER = "C:/Upload" 
-ALLOWED_EXTENSIONS = {'txt','pdf','docx','png','jpg','jpeg'} 
+# new_filename = filename + '-edited.' + extension
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+
+UPLOAD_FOLDER = "C:/Upload"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'docx', 'png', 'jpg', 'jpeg'}
 
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -140,18 +159,15 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            # add code for conversion here
             convert_file(filename)
-            
-            return redirect(url_for('upload_file', name=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+            download(new_filename)
+
+        return redirect(url_for('upload_file', name=filename))
+    return render_template('index.html')
+
+
+def download(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=True)
+
 
 app.run(debug=True, host="localhost")
